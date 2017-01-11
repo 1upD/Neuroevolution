dc99de0ccc2b2b938a6e7176a38263e867ce361c
CRRDerek <1upderek@gmail.com>
2017-01-11 13:17:24 -0500

    Wrote the XOR game

--- Games/xor.go
+++ Games/xor.go
@@ -1,5 +1,9 @@
 package games
 
+import (
+	"math/rand"
+)
+
 // Because even Tic Tac Toe was too complicated for my earliest tests, I've come
 // up with a game that's even more trivial. It's called "the XOR game". Each
 // player is given two binary inputs and is expected to answer yes or no.
@@ -15,12 +19,56 @@ package games
 // move on to tic-tac-toe, then to connect four, then maybe Othello, and then if
 // I am extraordinairily ambitious, Go.
 
-func XorGame(Player, Player) int {
-	// Unimplemented
+func XorGame(p1 Player, p2 Player) int {
+	var a float64
+	var b float64
+	var c int
+
+	switch rand.Intn(4) {
+	case 0:
+		a = 0
+		b = 0
+		c = 0
+	case 1:
+		a = 0
+		b = 1
+		c = 1
+	case 2:
+		a = 1
+		b = 0
+		c = 1
+	case 3:
+		a = 1
+		b = 1
+		c = 1
+	}
+
+	p1move := p1([]float64{a, b}, []interface{}{0, 1})
+	p2move := p2([]float64{a, b}, []interface{}{0, 1})
+
+	if p1move == p2move {
+		return 0
+	}
+
+	if p1move == c {
+		return 1
+	}
+
+	if p2move == c {
+		return -1
+	}
+
 	return 0
 }
 
 func XorGamePlayerMaker(a Agent) Player {
-	// Unimplemented
-	return nil
+	return func(game_state interface{}, moves []interface{}) interface{} {
+		prediction := a.Predict(game_state.([]float64))
+		if prediction[0] > prediction[1] {
+			return 1
+		} else {
+			return 0
+		}
+
+	}
 }
