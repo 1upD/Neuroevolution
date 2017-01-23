d4f30189841908963b35200c82247b929bdee281
CRRDerek <1upderek@gmail.com>
2017-01-22 21:48:15 -0500

    Exported neuron and network fields to fix JSON encoding

--- neuralnetwork/NeuralNetwork.go
+++ neuralnetwork/NeuralNetwork.go
@@ -13,17 +13,17 @@ import (
 // rate or threshold as would be expected from a typical backpropogated neural
 // network.
 type neuralNetwork struct {
-	num_inputs  int
-	num_hiddens int
-	num_outputs int
+	Num_inputs  int
+	Num_hiddens int
+	Num_outputs int
 
 	activations_hidden []chan float64
 	activations_output []chan float64
 
 	hidden_outputs []float64
 
-	layer_hidden []*neuron
-	layer_output []*neuron
+	Layer_hidden []*neuron
+	Layer_output []*neuron
 }
 
 // Predict() feeds a given input array into the network and activates the neurons.
@@ -32,11 +32,11 @@ type neuralNetwork struct {
 // The output array will match the output size of the network.
 func (net neuralNetwork) Predict(inputs []float64) []float64 {
 	// Launch a goroutine for each hidden neuron to calculate the output.
-	for i := 0; i < net.num_hiddens; i++ { // TODO Why is this minus one!?
+	for i := 0; i < net.Num_hiddens; i++ { // TODO Why is this minus one!?
 		index := i
 		go func() {
 			//			fmt.Println("Preparing to send hidden neuron ", index)
-			net.activations_hidden[index] <- net.layer_hidden[index].Activate(inputs)
+			net.activations_hidden[index] <- net.Layer_hidden[index].Activate(inputs)
 			//			fmt.Println("Preparing to send hidden neuron ", index)
 		}()
 	}
@@ -44,25 +44,25 @@ func (net neuralNetwork) Predict(inputs []float64) []float64 {
 	// Make a new slice to store the final outputs.
 	// This slice can't be reused like the hidden outputs because we need
 	// to pass by value.
-	final_outputs := make([]float64, net.num_outputs)
+	final_outputs := make([]float64, net.Num_outputs)
 
 	// Receive values from earlier goroutines.
-	for j := 0; j < net.num_hiddens; j++ {
+	for j := 0; j < net.Num_hiddens; j++ {
 		//		fmt.Println("Preparing to receive hidden neuron ", j)
 		net.hidden_outputs[j] = <-net.activations_hidden[j]
 		//		fmt.Println("Received hidden neuron ", j)
 	}
 
 	// Launch a goroutine for each output neuron to calculate the final output.
-	for i := 0; i < net.num_outputs; i++ { // TODO Why is this minus one!?
+	for i := 0; i < net.Num_outputs; i++ { // TODO Why is this minus one!?
 		index := i
 		go func() {
-			net.activations_output[index] <- net.layer_output[index].Activate(net.hidden_outputs)
+			net.activations_output[index] <- net.Layer_output[index].Activate(net.hidden_outputs)
 		}()
 	}
 
 	// Receive values from output goroutines.
-	for j := 0; j < net.num_outputs; j++ { // TODO Why is this minus one!?
+	for j := 0; j < net.Num_outputs; j++ { // TODO Why is this minus one!?
 		final_outputs[j] = <-net.activations_output[j]
 	}
 
@@ -73,31 +73,31 @@ func (net neuralNetwork) Predict(inputs []float64) []float64 {
 // Generate a single hidden layer neural network with randomly assigned weights.
 // This will be used at the beginning of an evolutionary algorithm to randomly
 // seed the population.
-func RandomNetwork(num_inputs, num_hiddens, num_outputs int) games.Agent {
+func RandomNetwork(num_inputs, Num_hiddens, Num_outputs int) games.Agent {
 	n := new(neuralNetwork)
-	n.num_inputs = num_inputs
-	n.num_hiddens = num_hiddens
-	n.num_outputs = num_outputs
+	n.Num_inputs = num_inputs
+	n.Num_hiddens = Num_hiddens
+	n.Num_outputs = Num_outputs
 
-	n.activations_hidden = make([]chan float64, num_hiddens)
-	n.activations_output = make([]chan float64, num_outputs)
+	n.activations_hidden = make([]chan float64, Num_hiddens)
+	n.activations_output = make([]chan float64, Num_outputs)
 
-	n.hidden_outputs = make([]float64, num_hiddens)
+	n.hidden_outputs = make([]float64, Num_hiddens)
 
-	n.layer_hidden = make([]*neuron, num_hiddens)
-	n.layer_output = make([]*neuron, num_outputs)
+	n.Layer_hidden = make([]*neuron, Num_hiddens)
+	n.Layer_output = make([]*neuron, Num_outputs)
 
 	// Initialize hidden activation channels and hidden neurons
-	for i := 0; i < num_hiddens; i++ {
+	for i := 0; i < Num_hiddens; i++ {
 		n.activations_hidden[i] = make(chan float64)
-		n.layer_hidden[i] = RandomNeuron(num_inputs)
+		n.Layer_hidden[i] = RandomNeuron(num_inputs)
 		n.hidden_outputs[i] = 0.0
 	}
 
 	// Initialize output activation channels and output neurons
-	for i := 0; i < num_outputs; i++ {
+	for i := 0; i < Num_outputs; i++ {
 		n.activations_output[i] = make(chan float64)
-		n.layer_output[i] = RandomNeuron(num_hiddens)
+		n.Layer_output[i] = RandomNeuron(Num_hiddens)
 	}
 
 	return n
@@ -115,20 +115,20 @@ func (n neuralNetwork) Mate(other games.Agent) games.Agent {
 // When two neural networks love each other very much...
 func mate(p1 *neuralNetwork, p2 *neuralNetwork) *neuralNetwork {
 	n := new(neuralNetwork)
-	n.num_inputs = p1.num_inputs
-	n.num_hiddens = p1.num_hiddens
-	n.num_outputs = p1.num_outputs
+	n.Num_inputs = p1.Num_inputs
+	n.Num_hiddens = p1.Num_hiddens
+	n.Num_outputs = p1.Num_outputs
 
-	n.activations_hidden = make([]chan float64, p1.num_hiddens)
-	n.activations_output = make([]chan float64, p1.num_outputs)
+	n.activations_hidden = make([]chan float64, p1.Num_hiddens)
+	n.activations_output = make([]chan float64, p1.Num_outputs)
 
-	n.hidden_outputs = make([]float64, p1.num_hiddens)
+	n.hidden_outputs = make([]float64, p1.Num_hiddens)
 
-	n.layer_hidden = make([]*neuron, p1.num_hiddens)
-	n.layer_output = make([]*neuron, p1.num_outputs)
+	n.Layer_hidden = make([]*neuron, p1.Num_hiddens)
+	n.Layer_output = make([]*neuron, p1.Num_outputs)
 
 	// Initialize hidden activation channels and hidden neurons
-	for i := 0; i < p1.num_hiddens; i++ {
+	for i := 0; i < p1.Num_hiddens; i++ {
 		n.activations_hidden[i] = make(chan float64)
 		n.hidden_outputs[i] = 0.0
 
@@ -136,39 +136,39 @@ func mate(p1 *neuralNetwork, p2 *neuralNetwork) *neuralNetwork {
 		randomFloat := rand.Float64()
 		// If the random value is less than 0.5, choose the mother
 		if randomFloat < 0.5 {
-			n.layer_hidden[i] = CopyNeuron(p1.layer_hidden[i])
+			n.Layer_hidden[i] = CopyNeuron(p1.Layer_hidden[i])
 			// If the random value is greater than 0.5 but less than 0.995, choose
 			// the father.
 		} else {
-			n.layer_hidden[i] = CopyNeuron(p2.layer_hidden[i])
+			n.Layer_hidden[i] = CopyNeuron(p2.Layer_hidden[i])
 		}
 	}
 
 	// Initialize output activation channels and output neurons
-	for i := 0; i < p1.num_outputs; i++ {
+	for i := 0; i < p1.Num_outputs; i++ {
 		n.activations_output[i] = make(chan float64)
 
 		// Get a random float to determine the parent of this gene
 		randomFloat := rand.Float64()
 		// If the random value is less than 0.5, choose the mother
 		if randomFloat < 0.5 {
-			n.layer_output[i] = CopyNeuron(p1.layer_output[i])
+			n.Layer_output[i] = CopyNeuron(p1.Layer_output[i])
 			// If the random value is greater than 0.5 but less than 0.995, choose
 			// the father.
 		} else {
-			n.layer_output[i] = CopyNeuron(p2.layer_output[i])
+			n.Layer_output[i] = CopyNeuron(p2.Layer_output[i])
 		}
 	}
 
 	// Check for mutation
 	randomFloat := rand.Float64()
 	if randomFloat > 0.8 {
-		gene := rand.Intn(n.num_hiddens + n.num_outputs)
-		if gene >= n.num_hiddens {
-			gene -= n.num_hiddens
-			n.layer_output[gene] = RandomNeuron(n.num_hiddens)
+		gene := rand.Intn(n.Num_hiddens + n.Num_outputs)
+		if gene >= n.Num_hiddens {
+			gene -= n.Num_hiddens
+			n.Layer_output[gene] = RandomNeuron(n.Num_hiddens)
 		} else {
-			n.layer_hidden[gene] = RandomNeuron(n.num_inputs)
+			n.Layer_hidden[gene] = RandomNeuron(n.Num_inputs)
 		}
 
 	}
@@ -202,5 +202,22 @@ func LoadJSON(filepath string) (*neuralNetwork, error) {
 	n := neuralNetwork{}
 
 	err = enc.Decode(&n)
+
+	n.activations_hidden = make([]chan float64, n.Num_hiddens)
+	n.activations_output = make([]chan float64, n.Num_outputs)
+
+	n.hidden_outputs = make([]float64, n.Num_hiddens)
+
+	// Initialize hidden activation channels
+	for i := 0; i < n.Num_hiddens; i++ {
+		n.activations_hidden[i] = make(chan float64)
+		n.hidden_outputs[i] = 0.0
+	}
+
+	// Initialize output activation channels and output neurons
+	for i := 0; i < n.Num_outputs; i++ {
+		n.activations_output[i] = make(chan float64)
+	}
+
 	return &n, err
 }
